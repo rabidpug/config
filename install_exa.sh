5499e168d6341f348d30184e8a7d36cbafdd57d7
Matt Cuneo <m@jcuneo.com>
2021-11-09 10:52:02 +1100

    omf installl

--- install_exa.sh
+++ install_exa.sh
@@ -4,9 +4,9 @@ cd tmp
 tag=$(curl -fsSL https://api.github.com/repos/ogham/exa/releases/latest | jq -r .tag_name)
 wget "https://github.com/ogham/exa/releases/download/$tag/exa-linux-x86_64-$tag.zip"
 unzip exa-linux-x86_64-$tag.zip
-mv bin/* /usr/local/bin
-mkdir -p /usr/share/fish/completions
-mv completions/* /usr/share/fish/completions/
-mv man/* /usr/share/man/man1/
+sudo mv bin/* /usr/local/bin
+sudo mkdir -p /usr/share/fish/completions
+sudo mv completions/* /usr/share/fish/completions/
+sudo mv man/* /usr/share/man/man1/
 cd ../
 rm -rf tmp
