51de345f98703b9ca3a511b283c849e3a87a0814
Matt Cuneo <m@jcuneo.com>
2021-11-10 13:34:32 +1100

    fzf fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
   fish_vi_key_bindings
   set -U fish_greeting "🐟"
   set -U FZF_COMPLETE 1
-  set -U FZF_LEGACY_KEYBINDINGS 0
   function fish_user_key_bindings
     bind -M insert \t '__fzf_complete'
   end
