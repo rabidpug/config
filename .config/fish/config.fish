4d800f7c6a3131eb71f44fd5b7d144b4ff18bc69
Matt Cuneo <m@jcuneo.com>
2021-12-28 14:13:59 +1100

    Add ctrl+left/right forward/backward-word

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
   fish_vi_key_bindings
+  bind \e\[1\;5C forward-word
+  bind \e\[1\;5D backward-word
   set -U fish_greeting "🐟"
   set -U FZF_COMPLETE 1
   function fish_user_key_bindings
